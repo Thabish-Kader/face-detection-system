e2c72e8439aaeed7985b455952c23d1afb4e82e6
Thabish <kadertabish2@gmail.com>
2023-12-31 11:54:41 +0530

    port updated
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 8080;
 export const FLASK_ENDPOINT = "http://127.0.0.1:5000/face_recognition";
